6714af9f5fbdcdbc8e1277af32ab6fa16846cf04
Chris Hopkins <itfobutaow@gmail.com>
2018-05-06 00:00:27 +0000

    Began WIP storage code

--- Assets/Scripts/Resources/Storehouse.cs
+++ Assets/Scripts/Resources/Storehouse.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class Storehouse : MonoBehaviour {
 
+    public enum Resource
+    {
+      VEGETABLE =0,
+      MEAT,
+      WATER,
+      WOOD,
+      NAILS,
+
+    };
+
+    public int maxVegetables = 10;
+    public int maxMeat = 10;
+    public int maxWater = 20;
+    public int maxWood = 20;
+    public int maxNails = 10;
+
+    //Tier 1
+
+        //Sustainance
+    public int vegetables = 0;
+    public int meat = 0;
+    public int water = 0;
+
+    //Building Resources
+    public int wood = 0;
+    public int nails = 0;
+    //TODO complete
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +43,54 @@ public class Storehouse : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    public int AddResourceToStorage(Resource _res, int quantity)
+    {
+        int remainder = 0;
+        switch ( _res)
+        {
+            case Resource.VEGETABLE:
+                {
+                    if ((vegetables + quantity) < maxVegetables)
+                    {
+                        vegetables += quantity;
+                        return 0;
+                    }
+                    else
+                    {
+                        //TODO loop to add based on quantity, return remainder;
+                        return remainder;
+                    }
+                    break;
+                }
+            case Resource.MEAT:
+                {
+                    return 0;
+                    break;
+                }
+            case Resource.WATER:
+                {
+                    return 0;
+                    break;
+                }
+            case Resource.WOOD:
+                {
+                    return 0;
+                    break;
+                }
+            case Resource.NAILS:
+                {
+                    return 0;
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Unkown Resource, case switch potentially incomplete.");
+                    return 0;
+                    break;
+                }
+        }
+    }
+
 }
