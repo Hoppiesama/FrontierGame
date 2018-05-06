c82ac04603cfd7c17a88d21b0097f9eb5508292c
hoppiesama <christopher3.hopkins@live.uwe.ac.uk>
2018-05-06 15:41:19 +0100

    Placeholder character, child objects with tags will be used to record who is resident quickly etc

---
+++ Assets/Scripts/TownStats/ResidentsStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResidentsStats : MonoBehaviour {
+
+    [Range (0.0f, 100.0f)]
+    public float lawfulness = 0.5f;
+
+    [Range(0.0f, 100.0f)]
+    public float happiness = 0.5f;
+
+    public int availableHomes = 0;
+    public int numberOfResidents = 0;
+
+    GameObject[] residents;
+
+    // Use this for initialization
+    void Start()
+    {
+        residents = GameObject.FindGameObjectsWithTag("Resident");
+        for (int i = 0; i < residents.Length; i++)
+        {
+            residents[i] = residents[i].transform.parent.gameObject;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
