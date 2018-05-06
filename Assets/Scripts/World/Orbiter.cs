e925ec1e0d62ba31f46f40855cd23eb9d18ea582
hoppiesama <christopher3.hopkins@live.uwe.ac.uk>
2018-05-06 23:19:45 +0100

    Added day night rotation using time scaling

---
+++ Assets/Scripts/World/Orbiter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour {
+
+    public float orbitSpeed = 0.0f;
+    private bool triggerLightOnce = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.right, orbitSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+
+        if (transform.position.y < -60.0f && triggerLightOnce == false)
+        {
+            GetComponent<Light>().enabled = false;
+            triggerLightOnce = true;
+        }
+        else if (transform.position.y > -60.0f && triggerLightOnce == true)
+        {
+            GetComponent<Light>().enabled = true;
+            triggerLightOnce = false;
+        }
+	}
+}
